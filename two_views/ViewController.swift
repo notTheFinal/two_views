f1ce9acedb10035b0b74fa153e891fbb7cd10cb1
JlblCblU4EPT <sashabarsasha228@yandex.ru>
2022-03-28 16:36:29 +0800

    Initial Commit

---
+++ two_views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  two_views
+//
+//  Created by Александр on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
